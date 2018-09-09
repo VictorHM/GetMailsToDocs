f80fe00f36850e62f77d3df861066cdae2310f64
Victor <rcpoetics@gmail.com>
2018-09-09 11:56:45 +0200

    Update comments and var names
    Updated comments for clarity. Rename vars and strings to make script generic

--- MailsToDocs.js
+++ MailsToDocs.js
@@ -4,10 +4,9 @@
  * as a way to get information from newsletters in a single document
  */
 function getMailsToDoc() {
-  // Create a new Google Doc named 'Hello, world!'
-  //TODO: check if the document already exists, if not, creates it
+  //Creates the document to contains extracted mail texts
   var date = Utilities.formatDate(new Date(), "GMT", "dd-MM-yyyy");
-  var doc = DocumentApp.create('Alexander mails - Helth and Sport ' + date);
+  var doc = DocumentApp.create('NewDocument ' + date);
   
   // Define a custom paragraph style.
   var styleTitle = {};
@@ -20,26 +19,35 @@ function getMailsToDoc() {
   // Get the email address of the active user - that's you.
   var email = Session.getActiveUser().getEmail();
   
-  //var threads = GmailApp.search('from:Alexander', 0, 20);
-  var threads = GmailApp.search('from:Alexander');
-  Logger.log("Number of threads found from Alexander> " + threads.length);
-  var senderEmail = 'Alexander <admin@alexanderjuanantoniocortes.com>';
+  //Search for the specific mail address you want to extract mails from
+  var threads = GmailApp.search('from:NewsLetter');
+  //DEBUG Log number of threads found with that email address
+  Logger.log("Number of threads found from NewsLetter> " + threads.length);
+  
+  //This is the right format for the string used later for comparison
+  var senderEmail = 'NewsLetter <admin@newsletter.com>';
   
   for (var i = 0; i < threads.length; i++) {
     var messages = threads[i].getMessages()
-    Logger.log(messages[0].getPlainBody()); // Log contents of the body
+    //DEBUG Log contents of the mails bodies to debug traces
+    Logger.log(messages[0].getPlainBody());
+    .
     for (var j = 0; j < messages.length; j++) {
       if(senderEmail == messages[j].getFrom()){
-        Logger.log(messages[0].getFrom()); // Log contents of the body
+        //DEBUG Log address of sender to debug traces
+        Logger.log(messages[0].getFrom());
+        
         var title = doc.getBody().appendParagraph(messages[j].getSubject());
         title.setAttributes(styleTitle);
-        var cuerpo = doc.getBody().appendParagraph(messages[j].getPlainBody());
-        cuerpo.setAttributes(styleBody);
+        var bodytext = doc.getBody().appendParagraph(messages[j].getPlainBody());
+        bodytext.setAttributes(styleBody);
         doc.getBody().appendHorizontalRule();
       }
     }
     
     //Mark the mail thread as read
+    threads[i].markRead();
+    //Send the thread to trash once we get the text in the document
     threads[i].moveToTrash();    
   }
   doc.saveAndClose();
